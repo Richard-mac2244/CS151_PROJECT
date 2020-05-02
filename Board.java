efdf0b9899d9df1431d690359a704068adeb5272
Richard-mac2244 <37561559+Richard-mac2244@users.noreply.github.com>
2020-05-02 14:19:30 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,5 +1,7 @@
-package Tictac;
+import java.awt.Graphics;
 
 public interface Board {
-
+	public void drawBoard(Graphics g); //draws tic tac toe board
+	public void drawX(Graphics g, int x, int y); //draws X on board
+	public void drawO(Graphics g, int x, int y); //draws O on board
 }
